05b59cd6ce6decd11fff0fa3121aac2981be61dc
GabrielLukaA <gabriel_luka-amaral@estudante.sesisenai.org.br>
2023-12-12 07:02:54 -0300

    implementa algumas coisas

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -126,22 +126,19 @@ export default function Home() {
       audio.pause();
     }
 
-  const waves = document.querySelectorAll(".wave");
+    const waves = document.querySelectorAll(".wave");
 
-  // Adiciona a classe 'paused' a cada elemento
-  if (isNew){
-    waves.forEach((wave) => {
-      wave.classList.add("paused");
-    });
-  } else {
-    waves.forEach((wave) => {
-      wave.classList.toggle("paused");
-    });
+    // Adiciona a classe 'paused' a cada elemento
+    if (isNew) {
+      waves.forEach((wave) => {
+        wave.classList.add("paused");
+      });
+    } else {
+      waves.forEach((wave) => {
+        wave.classList.toggle("paused");
+      });
+    }
   }
-
-}
-   
-
 
   function pausa() {
     console.log("sadjhkasjdlkasj");
@@ -153,19 +150,11 @@ export default function Home() {
     });
   }
   return (
-    <div className="w-screen h-screen flex flex-col gap-8 px-5">
+    <div className="w-screen h-screen flex pt-24 flex-col gap-8 px-5">
       <div className="flex flex-col gap-4">
-        <h1 className="text-white">
+        <h1 className="text-white text20">
           Você está assistindo a{" "}
-          <b
-            className="text-[#78FF47]"
-            onClick={() => {
-              setCurrentMusic(blocks[1]);
-              pause(true, currentMusic.audio, true);
-            }}
-          >
-            Rádio Versátil!
-          </b>
+          <b className="text-[#78FF47] text20">Rádio Versátil!</b>
         </h1>
         <CurrentMusic
           isPlaying={isPlaying}
@@ -177,29 +166,31 @@ export default function Home() {
         />
       </div>
       <div className="flex flex-col gap-6">
-        <h2 className="text-white" onClick={() => pausa()}>
-          Ouça seu programa favorito
-        </h2>
+        <h2 className="text-white text16">Ouça seu programa favorito</h2>
         <div className="flex overflow-auto gap-6 w-full ">
-
-
-        {blocks.map((block) => {
-          if (block.visible && !block.principal) {
-            return (
-              <div>
-                <p className="min-w-[30%] w-[30%] whitespace-nowrap"
+          {blocks.map((block, index) => {
+            if (block.visible && !block.principal) {
+              return (
+                <div
+                  key={index}
+                  className="w-full oi bg-[#0e0e0e] rounded-t-2xl min-w-[97px] items-center justify-center flex  max-w-[98px] h-[100px] border-t-2 border-l-2 border-r-2 border-white"
                   onClick={() => {
                     setCurrentMusic(block);
                     pause(true, currentMusic.audio, true);
                   }}
                 >
-                  {block.name}
-                </p>
-              </div>
-            );
-          }
-        })}
+                  <img
+                    src={block.image}
+                    width={75}
+                    className="w-4/5"
+                    height={75}
+                    alt=""
+                  />
                 </div>
+              );
+            }
+          })}
+        </div>
       </div>
     </div>
   );
